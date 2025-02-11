f64178009fb7b5689b00eb7756712ad3f5447946
alaaMatouq <alaamatouq361@gmail.com>
2025-02-11 04:15:13 +0300

    welcome screens (5 welcome screens the first one contains navigation button to the second screen the second, third, fourth screens contains 2 buttons(next, skip) the skip navigate to dummy sign up page i will connect the correct page when it will be ready, the next navigate to the next page, the last page contain start button navigate to sign up page. all the 5 screens are in the same dart file(on boarding screens)

--- lib/src/features/authentication/screens/on_boarding/on_boarding_screens.dart
+++ lib/src/features/authentication/screens/on_boarding/on_boarding_screens.dart
@@ -11,6 +11,8 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       context,
       MaterialPageRoute(builder: (context) => WelcomeScreen2()),
     );
+
+
   }
 
   @override
@@ -106,13 +108,26 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
 }
 
 class WelcomeScreen2 extends StatelessWidget {
+  void _skipToSignUp(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SignUpScreen()),
+    );
+  }
+
+  void _goToNextScreen(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => welcomeScreen3()), // Replace with the actual next screen
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF5F5F5),
-      body:Stack(
+      body: Stack(
         children: [
-          // Background Image
           Positioned(
             top: 200,
             child: Image.asset(
@@ -120,10 +135,8 @@ class WelcomeScreen2 extends StatelessWidget {
               width: 400,
             ),
           ),
-
-          // Title Text
           Positioned(
-            top: 60, // Keeps the vertical position
+            top: 60,
             left: 20,
             right: 20,
             child: Center(
@@ -139,10 +152,8 @@ class WelcomeScreen2 extends StatelessWidget {
               ),
             ),
           ),
-
-          // Subtitle Text (Aligned with Title)
           Positioned(
-            top: 175, // Moves it closer to the title
+            top: 175,
             left: 30,
             right: 30,
             child: Center(
@@ -152,7 +163,7 @@ class WelcomeScreen2 extends StatelessWidget {
                     "travel experience tailored to your needs.",
                 textAlign: TextAlign.center,
                 softWrap: true,
-                maxLines: 3, // Prevents overflow issues
+                maxLines: 3,
                 style: TextStyle(
                   fontSize: 16,
                   fontFamily: 'Lato',
@@ -162,9 +173,503 @@ class WelcomeScreen2 extends StatelessWidget {
               ),
             ),
           ),
+
+          // Skip Button (Top-Right)
+          Positioned(
+            bottom: 37, // Positions the button correctly
+            right: 100,
+
+            child: GestureDetector(
+
+              onTap: () => _skipToSignUp(context),
+
+              child: Text(
+                "skip",style: TextStyle(
+                fontSize: 26,
+                fontFamily: 'Lato',
+                fontWeight: FontWeight.w900,
+                color: Color(0xFFF5F5F5),
+
+              ),
+              ),
+            ),
+          ),
+
+
+          Positioned(
+            bottom: 30, // Positions the button correctly
+            left: 100,
+            child: GestureDetector(
+              onTap: () => _goToNextScreen(context),
+              child: Container(
+                width: 120,
+                height: 44,
+                alignment: Alignment.center, // Centers the text inside the container
+                decoration: BoxDecoration(
+                  color: Color(0xFF083A40),
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Text(
+                  "Next",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Lato',
+                    fontWeight: FontWeight.w900,
+                    color: Color(0xFFF5F5F5),
+                  ),
+                ),
+              ),
+            ),
+          ),
+
         ],
       ),
+    );
+  }
+}
 
+// Dummy SignUpScreen (Replace this with your actual sign-up page)
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text(
+          "Sign Up Screen",
+          style: TextStyle(fontSize: 24, color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
+
+// Dummy AnotherScreen (Replace this with the actual next welcome screen)
+
+class welcomeScreen3 extends StatelessWidget {
+  void _skipToSignUp(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SignUpScreen()),
+    );
+  }
+
+  void _goToNextScreen(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => WelcomeScreen4()), // Replace with actual next screen
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF5F5F5),
+      body: Stack(
+        children: [
+          // Background Image
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              width: double.infinity,
+              height: 500, // Adjust height to match design
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/welcome3.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+
+          // White Card Container Positioned Over the Image
+          Positioned(
+            top: 400, // Adjust to overlap the image
+            left: 20,
+            right: 20,
+            child: Container(
+              height: 320,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withAlpha(30), // Softer shadow
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center, // Centers the text
+                  mainAxisAlignment: MainAxisAlignment.center, // Centers everything vertically
+                  children: [
+                    // Title
+                    Text(
+                      "Tell us your travel goals",
+                      textAlign: TextAlign.center, // Center aligns the text
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontFamily: 'Lato',
+                        fontWeight: FontWeight.w900,
+                        color: Color(0xFF083A40),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+
+                    // Subtitle Text
+                    Text(
+                      "We’ll recommend the best places just for you! Whether it’s adventure, relaxation, or business, we’ve got you covered.",
+                      textAlign: TextAlign.center, // Center aligns the text
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Lato',
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    SizedBox(height: 30),
+
+                    // Buttons Row (Next & Skip) aligned properly
+
+                    Positioned(
+                      bottom: 80, // Aligns buttons at the bottom
+                      left: 0,
+                      right: 0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center, // Centers both buttons
+                        children: [
+                          // Next Button
+                          GestureDetector(
+                            onTap: () => _goToNextScreen(context),
+                            child: Container(
+                              width: 120,
+                              height: 44,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                color: Color(0xFF083A40),
+                                shape: BoxShape.rectangle,
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: Text(
+                                "Next",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontFamily: 'Lato',
+                                  fontWeight: FontWeight.w900,
+                                  color: Color(0xFFF5F5F5),
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          SizedBox(width: 20), // Space between buttons
+
+                          // Skip Button
+                          GestureDetector(
+                            onTap: () => _skipToSignUp(context),
+                            child: Text(
+                              "Skip",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontFamily: 'Lato',
+                                fontWeight: FontWeight.w900,
+                                color: Color(0xFF083A40), // Adjusted to dark color for better contrast
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+//welcome screen 4
+
+
+class WelcomeScreen4 extends StatelessWidget {
+  void _skipToSignUp(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SignUpScreen()),
+    );
+  }
+
+  void _goToNextScreen(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => WelcomeScreen5()), // Replace with actual next screen
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF5F5F5),
+      body: Stack(
+        children: [
+          // Background Image
+          Positioned.fill(
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/welcome44.png'), // Ensure correct path
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+
+          // White Card Container Positioned Over the Image
+          Positioned(
+            bottom: 80, // Adjust to overlap the image
+            left: 20,
+            right: 20,
+            child: Container(
+              height: 300, // Adjust height for better spacing
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withAlpha(30), // Softer shadow
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center, // Centers content
+                  children: [
+                    // Title
+                    Text(
+                      "Stay organized with\nour integrated tools",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontFamily: 'Lato',
+                        fontWeight: FontWeight.w900,
+                        color: Color(0xFF083A40),
+                      ),
+                    ),
+                    // Subtitle Text
+                    SizedBox(height: 10),
+                    Text(
+                      "Budget tracker, translator, currency converter, weather updates  and more. Everything you need, all in one place!",
+                      textAlign: TextAlign.center, // Center aligns the text
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Lato',
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+
+
+
+                    SizedBox(height: 20),
+
+                    // Buttons Row (Next & Skip)
+                    Positioned(
+                      bottom: 80, // Aligns buttons at the bottom
+                      left: 0,
+                      right: 0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center, // Centers both buttons
+                        children: [
+                          // Next Button
+                          GestureDetector(
+                            onTap: () => _goToNextScreen(context),
+                            child: Container(
+                              width: 120,
+                              height: 44,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                color: Color(0xFF083A40),
+                                shape: BoxShape.rectangle,
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: Text(
+                                "Next",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontFamily: 'Lato',
+                                  fontWeight: FontWeight.w900,
+                                  color: Color(0xFFF5F5F5),
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          SizedBox(width: 20), // Space between buttons
+
+                          // Skip Button
+                          GestureDetector(
+                            onTap: () => _skipToSignUp(context),
+                            child: Text(
+                              "Skip",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontFamily: 'Lato',
+                                fontWeight: FontWeight.w900,
+                                color: Color(0xFF083A40), // Adjusted to dark color for better contrast
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+//welcome screen5
+
+class WelcomeScreen5 extends StatelessWidget {
+  void _navigateToSignUp(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SignUpScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF5F5F5),
+      body: Stack(
+        children: [
+          // Background Image
+          Positioned(
+            top: -200,
+            left: 0,
+            right: 0,
+            bottom: 0,
+
+            child: Container(
+              width: double.infinity,
+
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/welcome5.png'), // Ensure correct path
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+
+          // White Card Container Positioned Over the Image
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withAlpha(30), // Soft shadow effect
+                    spreadRadius: 2,
+                    blurRadius: 10,
+                    offset: Offset(0, -4),
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start, // Aligns text to the left
+                  children: [
+                    // Title
+                    Text(
+                      "Your journey\nbegins now!",
+                      textAlign: TextAlign.left, // Ensures text aligns left
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontFamily: 'Lato',
+                        fontWeight: FontWeight.w900,
+                        color: Color(0xFF083A40),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+
+                    // Subtitle Text
+                    Text(
+                      "Discover, plan, and explore with iTravel.\nLet's create unforgettable memories together.",
+                      textAlign: TextAlign.left, // Aligns text to the left
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Lato',
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    SizedBox(height: 25),
+
+                    // Start Button
+                    Align(
+                      alignment: Alignment.centerRight, // Aligns the button to the left
+                      child: ElevatedButton(
+                        onPressed: () => _navigateToSignUp(context),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF083A40),
+                          fixedSize: Size(120, 44), // Width: 120, Height: 44
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                        child: Text(
+                          "Start",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontFamily: 'Lato',
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
